5e61c323f8ea596a2453f94c1e2d0c13daf39761
Erick <erickluques@gmail.com>
2019-09-05 00:51:25 -0300

    Instalação de dependencias + alterações na index

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <title>Poo Aula 2B</title>
+    <title>Qual é a Nave?</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/node_module/bootstrap/dist/css/bootstrap.css">
     <style>
         h1 {
             text-align: center;
@@ -18,7 +17,7 @@
         }
 
         .campo {
-            width: 80%;
+            width: 100%;
             margin: 5px auto;
         }
 
@@ -43,19 +42,30 @@
         <div class="col-md-3"></div>
         <div class="col-md-6 box">
             <form action="functions.php" method="post" class="formulario">
-                <label class="col-md-2">Modelo: </label>
-                <input type="text" name="modelo" class="form-control campo col-md-10">
-                <label class="col-md-2">Cor: </label>
-                <input type="text" name="cor" class="form-control campo col-md-10">
-                <label class="col-md-2">Ano: </label>
-                <input type="text" name="ano" class="form-control campo col-md-10">
-                <label class="col-md-2">KMs: </label>
-                <input type="text" name="km" class="form-control campo col-md-10">
+                <div class="form-group">
+                    <label class="col-md-2">Modelo: </label>
+                    <input type="text" name="modelo" class="form-control campo col-md-10">
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2">Cor: </label>
+                    <input type="text" name="cor" class="form-control campo col-md-10">
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2">Ano: </label>
+                    <input type="text" name="ano" class="form-control campo col-md-10">
+                </div>
+                <div class="form-group">
+                    <label class="col-md-2">KMs: </label>
+                    <input type="text" name="km" class="form-control campo col-md-10">
+                </div>
                 <button type="submit" class="btn btn-primary">Enviar</button>           
             </form>
         </div>
         <div class="col-md-3"></div>
 
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="/node_modules/jquery/dist/jquery.js"></script>
+<script src="/node_modules/popper.js/dist/umd/popper.js"></script>
+<script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+
 </body>
 </html>
