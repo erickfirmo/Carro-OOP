5bc5e7e00ebc209b9ab8db34758e9d7b232e5e9b
Erick <erickluques@gmail.com>
2019-09-12 00:56:59 -0300

    Métodos getters e setters da Classe carro

--- Carro.php
+++ Carro.php
@@ -4,63 +4,55 @@ class Carro {
     public $modelo;
     public $cor;
     public $ano_fabricacao;
-    public $ano_atual;
     public $anos_uso;
     public $km_rodados;
     public $km_ano;
     public $pagar_ipva;
-    public $dados_carro;
     
     public function __construct($m, $c, $a, $km) {
         $this->setModelo($m);
         $this->setCor($c);
-        $this->anoFabricacao($a);
-        $this->kmRodados($km);
-        $this->anosUso($a);
-        $this->kmRodados($km);
-        $this->kmAno($km, $a);
-        $this->ipva($a);
-        $this->dadosCarro($m, $c, $a, $km);
-
+        $this->setAnoFabricacao($a);
+        $this->setKmRodados($km);
+        $this->setAnosUso();
+        $this->setKmAno();
+        $this->setIpva();
     }
     
-    public function kmRodados($km) {
-       return $this->km_rodados = number_format($km, 0, ',','.');
-       
+    public function setKmRodados($km) {
+       $this->km_rodados = number_format($km, 0, ',','.');
     }
 
-    public function kmAno($km, $a) {
-        return $this->km_ano = number_format(($this->kmRodados($km) / $this->anosUso($a)), 0, ',', '.');
-        
+    public function getKmRodados() {
+        return $this->km_rodados;
+     }
+
+    public function setKmAno() {
+        $this->km_ano = number_format(($this->getKmRodados() / $this->getAnosUso()), 0, ',', '.'); 
     }
 
-    public function anoFabricacao($a) {
-        return $this->ano_fabricacao = $a;
+    public function getKmAno() {
+        return $this->km_ano; 
     }
 
-    public function anoAtual() {
-        return $this->ano_atual = date('Y');
+    public function setAnoFabricacao($a) {
+        $this->ano_fabricacao = $a;
     }
 
-    public function anosUso($a = null) {
-        
-        return $this->anos_uso = $this->anoAtual() - $this->anoFabricacao($a);
-
+    public function getAnoFabricacao() {
+        return $this->ano_fabricacao;
     }
 
-    public function ipva($a) {
-        $this->pagar_ipva = ($this->anosUso($a) < 10) ? true : false;
-
-        if ($this->pagar_ipva == true){
-            return "SIM";
-        }else{
-            return "NÃO";
-        }
-
+    public function setAnosUso() {
+        $this->anos_uso = date('Y') - $this->getAnoFabricacao();
     }
 
-    public function getAnoAtual() {
-        return $this->ano_atual;
+    public function setIpva() {
+        $this->pagar_ipva = ($this->getAnosUso() < 10) ? "SIM" : "NÃO";
+    }
+
+    public function getAnosUso() {
+        return $this->anos_uso;
     }
 
     public function getModelo() {
@@ -68,7 +60,7 @@ class Carro {
     }
 
     public function setModelo($m) {
-        return $this->modelo = $m;
+        $this->modelo = $m;
     }
 
     public function getCor() {
@@ -76,35 +68,25 @@ class Carro {
     }
 
     public function setCor($c) {
-        return $this->cor = $c;
+        $this->cor = $c;
     }
 
-    public function dadosCarro($m, $c, $a, $km) {
+    public function getIpva()
+    {
+        return $this->pagar_ipva;
+    }
 
-        return $this->dados_carro = array(
-            array(
-                'Modelo' => $this->getModelo()
-                ),
-            array(
-                'Cor' => $this->getCor()
-            ),
-            array(
-                'Ano de Fabricação' => $this->anoFabricacao($a)
-            ),
-            array(
-                'Anos de Uso' => $this->anosUso($a)
-            ),
-            array(
-                'Km(s) Rodados' => $this->kmRodados($km)
-            ),
-            array(
-                'Km(s) p/ Ano' => $this->kmAno($km, $a)
-            ),
-            array(
-                'Pagar IPVA' => $this->ipva($a)
-                )
+    public function dadosCarro() {
 
-        );
+        return [
+            'Modelo' => $this->getModelo(),
+            'Cor' => $this->getCor(),
+            'Ano de Fabricação' => $this->getAnoFabricacao(),
+            'Anos de Uso' => $this->getAnosUso(),
+            'Km(s) Rodados' => $this->getKmRodados(),
+            'Km(s) p/ Ano' => $this->getKmAno(),
+            'Pagar IPVA' => $this->getIpva()
+        ];
 
     }   
 
